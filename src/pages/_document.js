4c8fa90ea148690270cf06cd41cceb96ac236d92
jpaulfernandez <sirius@DESKTOP-2T9C1FR>
2023-04-18 16:12:14 +0800

    added analytics and refreshed api key

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head >
+      <Script strategy='afterInteractive' src="https://www.googletagmanager.com/gtag/js?id=G-YRP74J0TZZ" />
+      <Script strategy='afterInteractive' id='google-analytics' dangerouslySetInnerHTML={
+        {
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-YRP74J0TZZ');`
+        }
+      }/>
         <link
           href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap"
           rel="stylesheet"
