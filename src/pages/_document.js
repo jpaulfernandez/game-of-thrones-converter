fd8d00faeb90cbb09aee4b655ef473eefdeddbbc
jpaulfernandez <sirius@DESKTOP-2T9C1FR>
2023-04-18 15:19:33 +0800

    removed title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head >
-        <title>Maester's Manual</title>
         <link
           href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap"
           rel="stylesheet"
