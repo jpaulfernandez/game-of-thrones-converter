b55c0c683b4e72d0cc5c3879e1e1c5ed9eb88e61
jpaulfernandez <sirius@DESKTOP-2T9C1FR>
2023-04-18 18:24:31 +0800

    added meta data

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -19,6 +19,18 @@ export default function Document() {
           href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap"
           rel="stylesheet"
         />
+        <meta name="title" content="Maester's Manual" />
+        <meta name="description" content="Transform your words into the language of the Seven Kingdoms with just one click! Input any sentence, and watch as it's styled to sound like it was spoken in Game of Thrones" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://got.paulfernandez.dev/" />
+        <meta property="og:title" content="Maester's Manual" />
+        <meta property="og:description" content="Transform your words into the language of the Seven Kingdoms with just one click! Input any sentence, and watch as it's styled to sound like it was spoken in Game of Thrones" />
+        <meta property="og:image" content="/mm.png" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://got.paulfernandez.dev/" />
+        <meta property="twitter:title" content="Maester's Manual" />
+        <meta property="twitter:description" content="Transform your words into the language of the Seven Kingdoms with just one click! Input any sentence, and watch as it's styled to sound like it was spoken in Game of Thrones" />
+        <meta property="twitter:image" content="/mm.png" />
       </Head>
       <body className='bg-kings-landing'>
         <Main />
